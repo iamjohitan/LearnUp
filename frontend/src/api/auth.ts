21cf926bf64d4d0a98e105ecc7e48e0ab4a4fb4d
Esteban Marta <estebanmrojas13@gmail.com>
2025-11-11 23:14:57 -0500

    Inicio backend, front y conexion

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,43 @@
+import axiosClient from "./axiosClient";
+
+interface LoginCredentials{
+    email: string;
+    password: string;
+}
+
+interface RegisterCredentials extends LoginCredentials {
+    name: string;
+}
+
+interface RegisterSuccessResponse{
+    message: string;
+    emailSent: boolean;
+}
+
+interface AuthResponse {
+    token: string;
+    user: {id: string, email: string, name: string};
+}
+
+// Llamada al endpoint de login y guardar el token
+
+export const login = async(credentials: LoginCredentials): Promise<AuthResponse> => {
+    const response = await axiosClient.post<AuthResponse>('/auth/login', credentials)
+
+    //Si el login es existoso se guarda el token en el local storage
+    localStorage.setItem('accessToken', response.data.token);
+
+    return response.data
+};
+
+
+// Llama al endpoinjt de registro e inicia sesion si es exitoso
+
+export const register = async(credentials: RegisterCredentials): Promise<RegisterSuccessResponse> =>{
+    const response = await axiosClient.post<any>('/auth/register', credentials);
+
+    return{
+        message: 'Registro exitoso. Revisa tu correo electronico para verificar',
+        emailSent: true
+    }
+};
