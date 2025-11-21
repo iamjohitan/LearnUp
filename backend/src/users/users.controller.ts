b04a36dcfe05ffe587279848b2fc2269322a4b3a
Esteban Marta <estebanmrojas13@gmail.com>
2025-11-21 16:05:47 -0500

    Arreglo endpoint facultad y creacion endpoint acceder a data de user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Patch, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Patch, Body, UseGuards, Req, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import {AuthGuard} from "../auth/auth.guard"
+import { UpdateFacultyDto } from './dto/update-faculty-dto';
+import { get } from 'http';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
-    @UseGuards(AuthGuard)
     @Patch('faculty')
-    async updateFaculty(@Req() req, @Body('faculty_id') facultyId: string){
-        const userId = req.user.id
-        return this.usersService.updateFaculty(userId, facultyId)
+    @UseGuards(AuthGuard)
+    async updateFaculty(
+        @Body() dto: UpdateFacultyDto,
+        @Req() req,
+    ){
+        console.log('Controller - req.user:', req.user);
+        console.log('Controller - body:', dto);
+        return this.usersService.updateFaculty(req.user.id, dto.facultyId ?? null);
     }
-}
+
+    @Get('me')
+    @UseGuards(AuthGuard)
+    async getProfile(@Req() req ){
+        return this.usersService.getProfile(req.user.id);
+    }
+    }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -3,15 +3,57 @@ import supabase from 'src/infra/supabase.client';
 
 @Injectable()
 export class UsersService {
-    async updateFaculty(userId: string, facultyId: string){
-        if(!facultyId) throw new BadRequestException("El ID de la facultad es requerido");
+    async getProfile(userId: string){
+        const {data, error} = await supabase
+        .from('profiles')
+        .select(`
+            id,
+            name,
+            email,
+            faculty_id,
+            role_id
+            `)
+            .eq('id',userId)
+            .single();
 
+        if (error){
+            throw new BadRequestException("No se pudo obtener el perfil")
+        }
+        return data;
+    }
+
+
+    async updateFaculty(userId: string, facultyId: string | null){
+
+
+        if(!facultyId) throw new BadRequestException("El ID de la facultad es requerido");
+        
+        const { data: currentUser, error: selectError } = await supabase
+            .from('profiles')
+            .select('faculty_id')
+            .eq('id', userId)
+            .single();
+
+        if (selectError)
+            throw new BadRequestException("No se pudo obtener el usuario");
+
+        if (currentUser.faculty_id !== null) {
+            throw new BadRequestException("Este usuario ya tiene una facultad asignada y no se puede cambiar.");
+        }
         const {data, error} = await supabase
             .from('profiles')
             .update({faculty_id:facultyId})
             .eq('id', userId)
-            .select('id, faculty_id')
-            .single();
+            .select(
+                `
+                id,
+                name,
+                email,
+                faculty_id,
+                role_id
+                `
+            )
+            .maybeSingle();
 
         if (error) throw new BadRequestException(error.message);
 
