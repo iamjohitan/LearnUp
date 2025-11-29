b378ddf1855c340426680d8661a8d2ae7dc25edf
ErickDussanVelasco <erickcamilodussanvelasco@gmail.com>
2025-11-29 00:09:05 -0500

    talwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import LoginPage from './pages/LoginPage.tsx'
-import RegisterPage from './pages/RegisterPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RegisterPage />
+    <App />
   </StrictMode>,
 )
